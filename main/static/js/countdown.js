d930c2dba44f8a65ff39047643f7fe6bf107485d
enespinar <enespinar2@icloud.com>
2024-03-09 14:21:06 +0300

    countdown script added

---
+++ main/static/js/countdown.js
@@ -0,0 +1,24 @@
+var countDownDate = new Date("May 14, 2024 10:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("eventCountdownDay").innerHTML = days.toString();
+  document.getElementById("eventCountdownHour").innerHTML = hours.toString();
+  document.getElementById("eventCountdownMinute").innerHTML = minutes.toString();
+  document.getElementById("eventCountdownSecond").innerHTML = seconds.toString();
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("eventCountdownTitle").style.display = "none";
+    document.getElementById("eventCountdown").innerHTML = "Etkinlik Başladı!";
+  }
+}, 1000);
