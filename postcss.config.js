78d93fdc4bdb1076c46cc3164a940d7857fa85bf
roket1428 <meorhan@protonmail.com>
2024-03-12 01:29:13 +0300

    Added PostCSS build system with custom purge filters
    removed/renamed most static files (more to come)
    extracted carousel component from the flowbitejs

---
+++ postcss.config.js
@@ -0,0 +1,37 @@
+const ld = require('lodash')
+module.exports = {
+  plugins: {
+    '@fullhuman/postcss-purgecss': {
+      content: ['./main/templates/index.html'],
+      safelist: {
+        standard: [
+          /html/,
+          /body/,
+          /inset/,
+          /translate/,
+          /transform/
+        ],
+      },
+      extractors: [
+        {
+          // extractor: (content) => content.match(/[\w\-:.\/\[#%\]]+(?<!:)/g) || [],
+          extractor: (content) => {
+              // Capture as liberally as possible, including things like `h-(screen-1.5)`
+              const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+              const broadMatchesWithoutTrailingSlash = broadMatches.map((match) => ld.trimEnd(match, '\\'))
+
+              // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+              const innerMatches = content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || []
+
+              return broadMatches.concat(broadMatchesWithoutTrailingSlash).concat(innerMatches)
+          },
+          extensions: ["html"]
+        }
+      ]
+    },
+    'postcss-import': {},
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+  }
+}
