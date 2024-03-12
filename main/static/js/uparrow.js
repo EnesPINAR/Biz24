757dc36995ef07d13facee7624f705811e87dd72
BassicTR <>
2024-03-12 15:51:06 +0300

    Added scroll back to top button

---
+++ main/static/js/uparrow.js
@@ -0,0 +1,13 @@
+var toTopButton = document.getElementById("to-top-button");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        toTopButton.classList.remove("hidden");
+    } else {
+        toTopButton.classList.add("hidden");
+    }
+}
+
+function goToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
