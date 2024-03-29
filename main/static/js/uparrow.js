98c703e4d09e956a2e474ef87e7adb288e66c248
roket1428 <meorhan@protonmail.com>
2024-03-29 14:46:23 +0300

    Updated old events, added animation to scroll down indicator
    also fixed minor layout issues, replaced placeholders with real urls,
    formatted the template

--- main/static/js/uparrow.js
+++ main/static/js/uparrow.js
@@ -1,10 +1,13 @@
 var toTopButton = document.getElementById("to-top-button");
+var scrollDownIndicator = document.getElementById("scroll-down-indicator");
 
 window.onscroll = function () {
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         toTopButton.classList.remove("hidden");
+        scrollDownIndicator.style.opacity = 0;
     } else {
         toTopButton.classList.add("hidden");
+        scrollDownIndicator.style.opacity = 1;
     }
 }
 
